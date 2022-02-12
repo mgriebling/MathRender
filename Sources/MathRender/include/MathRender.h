dbb884760b07aef88a5976d94af429a0f06f09a5
Michael Griebling <sam@c-inspirations.com>
2022-02-12 08:58:46 -0500

    Back to original.

--- Sources/MathRender/include/MathRender.h
+++ Sources/MathRender/include/MathRender.h
@@ -8,8 +8,6 @@
 //  This software may be modified and distributed under the terms of the
 //  MIT license. See the LICENSE file for details.
 
-#define TARGET_OS_IPHONE 1
-
 #import <MTMathUILabel.h>
 #import <MTMathListDisplay.h>
 #import <MTMathList.h>

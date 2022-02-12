b81104deca0ec2cdbf368e6ef87a17dea3c9578f
Michael Griebling <sam@c-inspirations.com>
2022-02-12 09:28:41 -0500

    Added font resources.

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MathRender",
-    platforms: [
-        .macOS(.v10_10), .iOS(.v8)
-    ],
+//    platforms: [
+//        .macOS(.v10_10), .iOS(.v8)
+//    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -23,6 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MathRender",
+            resources: [
+                .copy("MathFontBundle.bundle")
+            ],
             dependencies: []),
         .testTarget(
             name: "MathRenderTests",

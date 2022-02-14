449ef3a5c014b9db44675d1259ca747bb6ed0e2a
Michael Griebling <sam@c-inspirations.com>
2022-02-14 07:39:03 -0500

    Updated the fonts installation.

--- Package.swift
+++ Package.swift
@@ -23,9 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MathRender",
-            dependencies: [],
+            dependencies: []),
+        .target(
+            name: "Shared",
             resources: [
-                .copy("mathFonts.bundle")
+                .copy("Resources")
             ]),
         .testTarget(
             name: "MathRenderTests",
